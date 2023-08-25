b3a50842644f2131b46750b4e6e78174430a4ad3
Feykicat23 <arsa.koshkin@gmail.com>
2023-08-25 10:48:41 +0300

    isAValidMail function without using a dictionary

--- public/assets/validMail.js
+++ public/assets/validMail.js
@@ -11,30 +11,26 @@ const isAValidMail = (mail) => {
     }
   }
 
-  const cantStartsandEnd = ['.', '@', '-', '"', "'", '(', ')', '_', '=', '+'];
-
-  const popularEndsOfMail = [
-    '@gmail.com', '@mail.ru', '@yandex.ru', '@yahoo.com', '@apple.com', '@protonmail.com', '@zoho.com', '@fastmail.com',
-  ];
-
-  const otherEndsOfMail = ['.com', '.net', '.org', '.gov', '.edu', '.info', '.us', '.ru', '.app', '.store',
+  const endsOfMail = ['.com', '.net', '.org', '.gov', '.edu', '.info', '.us', '.ru', '.app', '.store',
     '.club'];
 
-  const endOFmail = mail.slice(dogIndex);
+  const endOfMail = mail.slice(dogIndex);
 
   if (dogIndex === -1 || mail.length > 63) {
     return false;
-  } if (mail.includes('..') || mail.includes(' ')) {
+  } 
+  if (mail.endsWith('@') || mail.startsWith('@')){
+    return false
+  }
+  if (mail.includes('..') || mail.trim().includes(' ')) {
     return false;
-  } if (cantStartsandEnd.some((ending) => mail.endsWith(ending))
-               || cantStartsandEnd.some((start) => mail.startsWith(start))) {
+  } 
+  if (!endsOfMail.some((popularDomain) => mail.endsWith(popularDomain))) {
     return false;
-  } if (popularEndsOfMail.some((popularDomain) => mail.endsWith(popularDomain))) {
-    return true;
-  } if (!otherEndsOfMail.some((popularDomain) => mail.endsWith(popularDomain))) {
-    return false;
-  } if (endOFmail.length < 5) {
+  }
+  if (endOfMail.length < 5) {
     return false;
   }
   return true;
-};
+}
+
