e13bcaa9704c658c196cf738fa212bd7e854eec9
Feykicat23 <arsa.koshkin@gmail.com>
2023-10-18 18:52:02 +0300

    add PostgreSQL in express

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 const port = 3000;
 
 const html = fs.readFileSync('public/main.html', 'utf8');
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
@@ -40,62 +41,5 @@ app.get('/posts', async (req, res) => {
   } catch (error) {
     console.error('Ошибка при запросе к базе данных:', error);
     res.status(500).json({ error: 'Произошла ошибка при получении данных из базы данных' });
-  }
-});
-
-app.use(bodyParser.json()); // Парсим JSON-данные из запросов
-
-// Метод для создания поста
-app.post('/posts.json', (req, res) => {
-  const newPost = req.body;
-  posts.push(newPost);
-  res.status(201).json(newPost);
-});
-
-// Метод для удаления поста по ID
-app.delete('/posts/:id.json', (req, res) => {
-  const postId = req.params.id;
-  if (posts[postId]) {
-    posts.splice(postId, 1);
-    res.status(204).send();
-  } else {
-    res.status(404).json({ error: 'Пост с указанным ID не найден' });
-  }
-});
-
-// Метод для редактирования поста по ID
-app.post('/posts/:id.json', (req, res) => {
-  const postId = req.params.id;
-
-  if (posts[postId]) {
-    const updatedPost = req.body;
-    posts[postId] = updatedPost;
-    res.status(200).json(updatedPost);
-  } else {
-    res.status(404).json({ error: 'Пост с указанным ID не найден' });
-  }
-});
-
-// Эндпоинт для создания пользователя
-app.post('/createUser', async (req, res) => {
-  const { username, email, password } = req.body;
-
-  try {
-    // Проверка на существование пользователя с таким email или username в базе данных
-    const checkUserQuery = 'SELECT * FROM createUser WHERE email = $1';
-    const { rows } = await pool.query(checkUserQuery, [email]);
-
-    if (rows.length > 0) {
-      return res.status(400).json({ error: 'Пользователь с таким email уже существует' });
-    }
-
-    // Создание нового пользователя
-    const createUserQuery = 'INSERT INTO createUser (username, email, password) VALUES ($1, $2, $3)';
-    await pool.query(createUserQuery, [username, email, password]);
-
-    return res.status(200).json({ message: 'Пользователь успешно создан' });
-  } catch (error) {
-    console.error('Ошибка при создании пользователя:', error);
-    return res.status(500).json({ error: 'Произошла ошибка при создании пользователя' });
   }
 });
